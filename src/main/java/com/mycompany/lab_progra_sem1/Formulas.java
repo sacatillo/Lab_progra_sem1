04cd07aa26c65393aa3e317e7addf9f87f42833b
sacatillo <albertocarias110@gmail.com>
2024-10-18 10:58:49 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/lab_progra_sem1/Formulas.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab_progra_sem1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Martin Montes
+ */
+public class Formulas { 
+
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para obtener la entrada del usuario,AMMM
+        Scanner scanner = new Scanner(System.in);
+        
+        // Solicitar numerador y denominador para la primera fracción
+        System.out.print("Numerador 1: ");
+        double numerador1 = scanner.nextDouble();
+        
+        System.out.print("Denominador 1: ");
+        double denominador1 = scanner.nextDouble();
+        
+        // Solicitar numerador y denominador para la segunda fracción
+        System.out.print("Numerador 2: ");
+        double numerador2 = scanner.nextDouble();
+        
+        System.out.print("Denominador 2: ");
+        double denominador2 = scanner.nextDouble();
+        
+        // Calcular el resultado de la suma de las fracciones
+        double resultado = (numerador1 / denominador1) + (numerador2 / denominador2);
+        
+        // Mostrar el resultado
+        System.out.printf("El resultado es: %.2f\n", resultado);
+    }
+}
+
